4f986d5338894fbc264d8be92ae185ffbd56949a
ramonrrp <53453331+ramonrrp@users.noreply.github.com>
2021-10-17 21:24:13 -0300

    update kubernetes key

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ swapoff -a
 apt update
 apt install -y apt-transport-https ca-certificates curl
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc>
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
